9fe2989ff167558872f902c477e5f5eb9c2c3a97
Lena Zvereva <zverolen@gmail.com>
2023-11-09 09:20:58 +0100

    Update and clean up tabs component including tests

--- src/components/tabList/TabList.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export default function TabList({ children }) {
-  return (<div role="tablist" data-testid="tablist">{ children }</div>)
-}
-
-TabList.propTypes = {
-  children: PropTypes.element.isRequired
-}
--- src/components/tabsComponent/TabsComponent.cy.jsx
+++ src/components/tabsComponent/TabsComponent.cy.jsx
@@ -1,12 +1,12 @@
 import TabsComponent from './TabsComponent'
 
 const tabsReference = [
-  { id: '0', caption: 'Без ответа' },
-  { id: '1', caption: 'Знаю' },
-  { id: '2', caption: 'Учу' }
+  "Без ответа",
+  "Знаю",
+  "Учу"
 ]
 
-const tabpanelReference = [
+const tabpanelReference1 = [
   {
       "id": "1",
       "serbian": "Da li je ovo tvoja knjiga?",
@@ -58,62 +58,156 @@ const tabpanelReference = [
   }
 ]
 
+const tabpanelReference2 = [
+  {
+      "id": "2",
+      "serbian": "To nije njegov sat.",
+      "russian": "Это не его часы.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "3",
+      "serbian": "To je moje dete.",
+      "russian": "Это мой ребёнок.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "4",
+      "serbian": "To je tvoja sestra.",
+      "russian": "Это твоя сестра.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "5",
+      "serbian": "To je njegova kuća.",
+      "russian": "Это его дом.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "6",
+      "serbian": "Ovo je moj dobar drug.",
+      "russian": "Это мой хороший друг.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "7",
+      "serbian": "Šta je tvoja sestra?",
+      "russian": "Кем работает твоя сестра?",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  }
+]
+
+const tabpanelReference3 = [
+  {
+      "id": "1",
+      "serbian": "Da li je ovo tvoja knjiga?",
+      "russian": "Это твоя книга?",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "2",
+      "serbian": "To nije njegov sat.",
+      "russian": "Это не его часы.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "3",
+      "serbian": "To je moje dete.",
+      "russian": "Это мой ребёнок.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "4",
+      "serbian": "To je tvoja sestra.",
+      "russian": "Это твоя сестра.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  },
+  {
+      "id": "5",
+      "serbian": "To je njegova kuća.",
+      "russian": "Это его дом.",
+      "section": "R1",
+      "selfCheckStatus": "unset"
+  }
+]
+
 describe('<TabsComponent />', () => {
   it('Renders correctly with the first tab active', () => {
     cy.mount(
-      <TabsComponent tabs={ tabsReference } tabpanels={ tabpanelReference } />
+      <TabsComponent 
+        tabs={ tabsReference }
+        withoutAnswerPhrases={ tabpanelReference1 }
+        correctPhrases={ tabpanelReference2 }
+        wrongPhrases={ tabpanelReference3 }
+        scoreAll='34'
+        scoreCorrect='2'
+        scoreWrong='4'
+        onCheckStatusChange={()=>{}}
+        updatedTab='correct'
+      />
     )
 
     cy.getByTest('tablist')
       .should('have.attr', 'role', 'tablist')
       .and('have.attr', 'aria-labelledby', 'tabs-heading')
-    cy.get('h2')
-      .should('have.text', 'Фразы на сербском')
-      .and('have.attr', 'id', 'tabs-heading')
 
     // Check that the first tab is active
-    cy.getByTest('tabpanel').should('have.text', 'CONTENT TABPANEL 1')
+    cy.get('[role="tabpanel"]').find('.cards-container').children().should('have.length', '10')
 
   })
 
   it('Navigation works correctly', () => {
     cy.mount(
-      <TabsComponent tabs={ tabsReference } tabpanels={ tabpanelReference } />
+      <TabsComponent 
+        tabs={ tabsReference }
+        withoutAnswerPhrases={ tabpanelReference1 }
+        correctPhrases={ tabpanelReference2 }
+        wrongPhrases={ tabpanelReference3 }
+        scoreAll='34'
+        scoreCorrect='2'
+        scoreWrong='4'
+        onCheckStatusChange={()=>{}}
+        updatedTab='correct'
+      />
     )
 
     //Select the second tab
-    cy.get('button:first-child').focus()
-    cy.get('button:nth-child(2)').focus()
+    cy.get('button[role="tab"]:first-child').focus()
+    cy.get('button[role="tab"]:nth-child(2)').focus()
 
-    cy.getByTest('tabpanel').should('have.text', 'CONTENT TABPANEL 2')
+    cy.get('[role="tabpanel"]').find('.cards-container').children().should('have.length', '9')
 
     //Select the third tab
-    cy.get('button:nth-child(3)').focus()
+    cy.get('button[role="tab"]:nth-child(3)').focus()
 
-    cy.getByTest('tabpanel').should('have.text', 'CONTENT TABPANEL 3')
+    cy.get('[role="tabpanel"]').find('.cards-container').children().should('have.length', '8')
 
     //Click the first tab
-    cy.get('button:nth-child(1)').click()
+    cy.get('button[role="tab"]:nth-child(1)').click()
 
-    cy.get('button:nth-child(1)').should('be.focused')
-    cy.getByTest('tabpanel').should('have.text', 'CONTENT TABPANEL 1')
-
-    //Click the second tab
-    cy.get('button:nth-child(2)').click()
-
-    cy.get('button:nth-child(2)').should('be.focused')
-    cy.getByTest('tabpanel').should('have.text', 'CONTENT TABPANEL 2')
+    cy.get('button[role="tab"]:nth-child(1)').should('be.focused')
+    cy.get('[role="tabpanel"]').find('.cards-container').children().should('have.length', '10')
 
     //Click the second tab
-    cy.get('button:nth-child(2)').click()
+    cy.get('button[role="tab"]:nth-child(2)').click()
 
-    cy.get('button:nth-child(2)').should('be.focused')
-    cy.getByTest('tabpanel').should('have.text', 'CONTENT TABPANEL 2')
+    cy.get('button[role="tab"]:nth-child(2)').should('be.focused')
+    cy.get('[role="tabpanel"]').find('.cards-container').children().should('have.length', '9')
 
-    //Click the second tab
-    cy.get('button:nth-child(3)').click()
+    //Click the third tab
+    cy.get('button[role="tab"]:nth-child(3)').click()
 
-    cy.get('button:nth-child(3)').should('be.focused')
-    cy.getByTest('tabpanel').should('have.text', 'CONTENT TABPANEL 3')
+    cy.get('button[role="tab"]:nth-child(3)').should('be.focused')
+    cy.get('[role="tabpanel"]').find('.cards-container').children().should('have.length', '8')
   })
 })
