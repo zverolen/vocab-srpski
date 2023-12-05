5f9c2491cd2ec1ee3f57ae15e5530cbaf6ca8619
Lena Zvereva <zverolen@gmail.com>
2023-12-05 07:09:27 +0100

    Update disclosure test

--- src/components/disclosure/Disclosure.cy.jsx
+++ src/components/disclosure/Disclosure.cy.jsx
@@ -11,14 +11,20 @@ describe('<Disclusure />', () => {
     cy.getByTest('button-with-icon')
       .should('have.text', 'Инструкция')
       .and('have.attr', 'aria-expanded', 'false')
+    cy.getByTest('button-with-icon').find('span svg')
+      .should('have.class', 'plus')
     
 
     cy.getByTest('button-with-icon').click()
     cy.getByTest('disclosure').find('p').should('exist')
     cy.getByTest('disclosure').should('have.text', 'ИнструкцияИнструкцияSample content')
     cy.getByTest('button-with-icon').should('have.attr', 'aria-expanded', 'true')
+    cy.getByTest('button-with-icon').find('span svg')
+      .should('have.class', 'minus')
 
     cy.getByTest('button-with-icon').click()
     cy.getByTest('disclosure').should('have.text', 'ИнструкцияИнструкция')
+    cy.getByTest('button-with-icon').find('span svg')
+      .should('have.class', 'plus')
   })
 })
